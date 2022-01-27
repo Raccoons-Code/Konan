57016b2b05fcaf70792803b6ccc92a4980b0bf51
Gorniaky <hwss.s2@gmail.com>
2022-01-27 19:40:28 -0300

    deploy of application commands improved

--- src/bot/deploy-commands.js
+++ src/bot/deploy-commands.js
@@ -2,22 +2,22 @@ require('dotenv').config();
 
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { env: { CLIENT_ID, GUILD_ID, DISCORD_TOKEN } } = process;
+const { env: { CLIENT_ID, GUILD_ID, DEPLOY_GLOBAL, DEPLOY_RESET, DISCORD_TOKEN } } = process;
 const Commands = require('./commands');
 
-const GLOBAL = false;
-const reset = false;
+const GLOBAL = DEPLOY_GLOBAL == 'true';
+const reset = DEPLOY_RESET == 'true';
 
 const commands = [];
 const data = [];
 
-const { applicationCommands } = Commands.init();
+const { applicationCommands } = Commands;
 
-if (!reset)
+if (!reset) {
   Object.values(applicationCommands).forEach(e => commands.push(e.toJSON()));
 
-if (!reset)
   commands.flat().forEach(e => data.push(e.data.toJSON()));
+}
 
 const rest = new REST({ version: '9' }).setToken(DISCORD_TOKEN);
 
