964fa66ca74c0c5e73e092822486e1981766fff2
Gorniaky <hwss.s2@gmail.com>
2022-07-30 19:12:59 -0300

    New emojis added

--- src/util/Emoji/index.ts
+++ src/util/Emoji/index.ts
@@ -1,9 +1,14 @@
 const Emoji = new class Emoji {
+  [x: string]: string;
   Danger = '❌';
   Error = '❌';
   Info = '💬';
   Success = '✅';
   Warning = '⚠️';
+  online = '🟢';
+  offline = '⚫';
+  idle = '🟠';
+  dnd = '🔴';
 };
 
 export default Emoji;
