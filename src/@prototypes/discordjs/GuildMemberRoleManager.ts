58f2cf9b80d88d2b24620fbcce84922896b05eaa
Gorniaky <hwss.s2@gmail.com>
2022-07-30 22:24:21 -0300

    Type of `GuildMemberRoleManager` prototype updated

--- src/@prototypes/discordjs/GuildMemberRoleManager.ts
+++ src/@prototypes/discordjs/GuildMemberRoleManager.ts
@@ -1,8 +1,8 @@
-import { GuildMemberRoleManager } from 'discord.js';
+import { GuildMemberRoleManager, Role } from 'discord.js';
 
 Object.defineProperty(GuildMemberRoleManager.prototype, 'lowest', {
   get: function () {
-    return this.cache.reduce((prev: any, role: any) =>
+    return this.cache.reduce((prev: Role, role: Role) =>
       (role.id !== role.guild.id) ?
         (role.comparePositionTo(prev) < 0 ? role : prev) :
         prev, this.cache.first());
