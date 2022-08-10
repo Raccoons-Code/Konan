ca692297ff6e082ae9432f5a12ad3a17fa782526
Gorniaky <hwss.s2@gmail.com>
2022-08-10 20:17:53 -0300

    `toSnakeCake` fix `Date` incorrect transform

--- src/util/toSnakeCase.ts
+++ src/util/toSnakeCase.ts
@@ -1,5 +1,6 @@
 export default function toSnakeCase(obj: any): any {
   if (typeof obj !== 'object' || !obj) return obj;
+  if (obj instanceof Date) return obj;
   if (Array.isArray(obj)) return obj.map(toSnakeCase);
   return Object.fromEntries(Object.entries(obj).map(([key, value]) => [snakeCase(key), toSnakeCase(value)]));
 }
