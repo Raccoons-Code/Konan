e3bc64f128b9d01029e162c2ae90ec8e2d7911de
Gorniaky <hwss.s2@gmail.com>
2022-08-11 10:38:24 -0300

    using `readfileSync` to read `translator` `resources`

--- src/translator/resources/index.ts
+++ src/translator/resources/index.ts
@@ -1,5 +1,5 @@
 import { GlobSync } from 'glob';
-import { readdirSync, statSync } from 'node:fs';
+import { readdirSync, readFileSync, statSync } from 'node:fs';
 import { posix } from 'node:path';
 import { Resources } from '../src';
 
@@ -10,9 +10,20 @@ const langs = readdirSync(`${__dirname}`).filter(f => statSync(`${__dirname}/${f
 for (let i = 0; i < langs.length; i++) {
   const lang = langs[i];
 
-  const { found } = new GlobSync(posix.resolve('src', 'translator', 'resources', lang, '*.json'));
+  const { found } = new GlobSync(posix.join(__dirname.replace(/\\/g, '/'), lang, '*.json'));
 
-  translations[lang] = found.reduce((acc, value) => ({ ...acc, ...require(value) }), {});
+  if (!translations[lang])
+    translations[lang] = {};
+
+  for (let j = 0; j < found.length; j++) {
+    try {
+      const json = JSON.parse(readFileSync(found[j], 'utf8'));
+
+      Object.assign(translations[lang], json);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
 
 export default { ...translations };
