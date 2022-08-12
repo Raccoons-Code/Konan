b088e6d3687ac6c1578744e8c3f00f123f163dc3
Gorniaky <hwss.s2@gmail.com>
2022-08-12 18:21:28 -0300

    type improved

--- src/util/isSameDate.ts
+++ src/util/isSameDate.ts
@@ -1,2 +1,2 @@
-export = (oldDate: number, newDate = Date.now()) =>
+export = (oldDate: string | number | Date, newDate: string | number | Date = Date.now()) =>
   new Date(oldDate).setHours(0, 0, 0, 0) === new Date(newDate).setHours(0, 0, 0, 0);
