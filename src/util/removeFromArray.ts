0e88924be03deadbfcc89a04e94857981c4c7217
Gorniaky <hwss.s2@gmail.com>
2022-08-14 12:12:52 -0300

    `removeFromArray` improved

--- src/util/removeFromArray.ts
+++ src/util/removeFromArray.ts
@@ -1,6 +1,9 @@
-export = <T = any>(array: T[], target: T) => {
+export = <T = any>(array: T[], target: T | T[]) => {
+  if (Array.isArray(target))
+    return array.filter(item => !target.includes(item));
+
   while (array.includes(target))
     array.splice(array.indexOf(target), 1);
 
   return array;
-};
+}
