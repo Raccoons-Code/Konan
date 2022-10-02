c7569807ffa4a5964b626c636ad64a0016a27661
Gorniaky <hwss.s2@gmail.com>
2022-10-02 14:29:16 -0300

    cache updated

--- src/modules/Cache/index.ts
+++ src/modules/Cache/index.ts
@@ -1,1 +1,6 @@
-export const cache = new Map();
+import { LimitedCollection } from 'discord.js';
+
+export const cache = new LimitedCollection({
+  maxSize: 100,
+  keepOverLimit: (v, k, c) => c.size > 100,
+});
