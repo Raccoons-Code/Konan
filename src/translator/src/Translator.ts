ece9670613eec3cd448221945bae4a1370a7ccb3
Gorniaky <hwss.s2@gmail.com>
2022-10-03 21:53:30 -0300

    feat: translator updated

--- src/translator/src/Translator.ts
+++ src/translator/src/Translator.ts
@@ -1,8 +1,8 @@
-import type { Options, Resources, TranslationData } from './@types';
+import type { Options, TranslationData } from './@types';
+import cache from './Cache';
 import { defaults } from './Defaults';
 
 export default class Translator {
-  resources?: Resources;
   translation!: Required<TranslationData>;
 
   constructor(options: Options) {
@@ -10,23 +10,20 @@ export default class Translator {
   }
 
   translate(key: string, options: Options) {
-    const keys = key.split(options.translation?.keySeparator ?? this.translation.keySeparator);
-
-    const fallbackLocale = this.resources?.[this.translation.fallbackLocale];
+    const fallbackLocale = cache.resources?.[this.translation.fallbackLocale];
 
     const locale = options.locale ?? this.translation.fallbackLocale;
 
     const noScape = options.translation?.noScape ?? this.translation.noScape;
 
-    const translation = this.resources?.[locale] ?? this.resources?.[locale.split(/_|-/)[0]];
+    const translation = cache.resources?.[locale] ?? cache.resources?.[locale.split(/_|-/)[0]];
 
-    key = keys.reduce<any>((acc, k) => {
-      const pluralKey = `${k}_${new Intl.PluralRules(locale).select(options.count ?? 0)}`;
+    return <string>key.split(options.translation?.keySeparator ?? this.translation.keySeparator)
+      .reduce<any>((acc, k) => {
+        const pluralKey = `${k}_${new Intl.PluralRules(locale).select(options.count ?? 0)}`;
 
-      return acc?.[pluralKey] ?? acc?.[k] ??
-        (noScape ? null : fallbackLocale?.[pluralKey] ?? fallbackLocale?.[k] ?? k);
-    }, translation);
-
-    return key;
+        return acc?.[pluralKey] ?? acc?.[k] ??
+          (noScape ? null : fallbackLocale?.[pluralKey] ?? fallbackLocale?.[k] ?? k);
+      }, translation);
   }
 }
