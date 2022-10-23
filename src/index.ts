ec572b7f3a8d987aa8b3e7dc8b3a4601b7783a62
Gorniaky <hwss.s2@gmail.com>
2022-10-23 11:18:36 -0300

    comments removed

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
 import { ShardingManager } from 'discord.js';
 import 'dotenv/config';
 import { join } from 'node:path';
-import { /* env, */ execArgv } from 'node:process';
-/* import { ShardingClient } from 'statcord.js'; */
+import { env, execArgv } from 'node:process';
+import { ShardingClient } from 'statcord.js';
 
 const fileExt = __filename.split('.').pop();
 
-const manager = new ShardingManager(join(__dirname, `bot.${fileExt}`), { execArgv });
+export const manager = new ShardingManager(join(__dirname, `bot.${fileExt}`), { execArgv });
 
-export { manager };
-
-/* if (env.STATCORD_KEY) {
+if (env.STATCORD_KEY) {
   const statcord = new ShardingClient({
     key: env.STATCORD_KEY,
     manager,
+    autopost: true,
+    postCpuStatistics: true,
+    postMemStatistics: true,
+    postNetworkStatistics: true,
   });
 
   statcord.on('autopost-start', () => console.log('Statcord autopost started.'));
@@ -21,7 +23,7 @@ export { manager };
   statcord.on('post', (status) => {
     if (status) return console.error('Statcord post failed:', status);
   });
-} */
+}
 
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
 
