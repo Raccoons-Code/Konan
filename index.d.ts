cdcfe4cb3c7d5881fa45b85c0922be8eba52e860
Gorniaky <hwss.s2@gmail.com>
2023-03-25 21:09:01 -0300

    rename `DISCORD_PUBLIC_KEY` to `DISCORD_APPLICATION_PUBLIC_KEY`

--- index.d.ts
+++ index.d.ts
@@ -2,13 +2,13 @@ declare namespace NodeJS {
   interface ProcessEnv {
     DATABASE_URL?: string
     DISCORD_APPLICATION_ID?: string
+    DISCORD_APPLICATION_PUBLIC_KEY?: string
     DISCORD_APPLICATION_SECRET?: string
     DISCORD_ERROR_CHANNEL?: string
     DISCORD_GUILD_ID?: string
     DISCORD_GUILD_INVITE?: string
     DISCORD_GUILD_INVITE_CHANNEL?: string
     DISCORD_LOG_CHANNEL?: string
-    DISCORD_PUBLIC_KEY?: string
     DISCORD_TEST_GUILD_ID?: string
     DISCORD_TOKEN?: string
     NODE_ENV?: "development" | "production"
