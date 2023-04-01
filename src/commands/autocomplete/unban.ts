c668773515e3fd28e48bb723d2dc9426b34c5285
Gorniaky <hwss.s2@gmail.com>
2023-04-01 18:40:44 -0300

    Re-create unban autocomplete

---
+++ src/commands/autocomplete/unban.ts
@@ -0,0 +1,42 @@
+import { ApplicationCommandOptionChoiceData, AutocompleteInteraction } from "discord.js";
+import ChatInputAutocomplete from "../../structures/ChatInputAutocomplete";
+
+export default class extends ChatInputAutocomplete {
+  constructor() {
+    super({
+      name: "unban",
+      appPermissions: ["BanMembers"],
+      userPermissions: ["BanMembers"],
+    });
+  }
+
+  async execute(
+    interaction: AutocompleteInteraction<"cached">,
+    res: ApplicationCommandOptionChoiceData[] = [],
+  ) {
+    const user = interaction.options.getString("user");
+    const pattern = RegExp(user ?? "", "i");
+
+    const bans = await interaction.guild.bans.fetch();
+
+    for (const ban of bans.values()) {
+      const name = [
+        ban.user.tag, " | ", ban.user.id,
+        ban.reason ? ` | Reason: ${ban.reason}` : "",
+      ].join("");
+
+      if (pattern.test(name)) {
+        res.push({
+          name: name.slice(0, 100),
+          value: `${ban.user.id}`,
+        });
+
+        if (res.length === 25) break;
+      }
+    }
+
+    await interaction.respond(res);
+
+    return;
+  }
+}
