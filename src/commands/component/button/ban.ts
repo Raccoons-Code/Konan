bdcf32354ed5c66eea5cecc74db55c20415de88f
Gorniaky <hwss.s2@gmail.com>
2023-04-01 18:41:05 -0300

    fix multi ban

--- src/commands/component/button/ban.ts
+++ src/commands/component/button/ban.ts
@@ -1,5 +1,6 @@
 import { ButtonInteraction, EmbedBuilder, GuildMember, User, userMention } from "discord.js";
 import ms from "ms";
+import { setTimeout as sleep } from "node:timers/promises";
 import ButtonCommand from "../../../structures/ButtonCommand";
 
 export default class extends ButtonCommand {
@@ -11,8 +12,18 @@ export default class extends ButtonCommand {
   }
 
   async execute(interaction: ButtonInteraction<"cached">) {
-    const banning: Promise<string | void | GuildMember | User>[] = [];
+    await interaction.update({
+      components: [],
+    });
+
+    const banning: Promise<void>[] = [];
+    const banned: (string | User | GuildMember)[] = [];
     const failed: string[] = [];
+    let index = 0;
+
+    await interaction.editReply({
+      content: "Banning...",
+    });
 
     for (const embed of interaction.message.embeds) {
       const ids = embed.description?.match(/\d{17,}/g) ?? [];
@@ -24,24 +35,47 @@ export default class extends ButtonCommand {
 
       const deleteMessageSeconds = ms(deleteField.value) / 1000;
 
-      banning.push(...ids.map(id =>
-        interaction.guild.bans.create(id, {
-          deleteMessageSeconds,
-          reason,
-        })
-          .catch(() => {
-            failed.push(userMention(id));
-          })));
+      banning.push(...ids.map(async (id, i) => {
+        await sleep((i + index) * 1000);
+
+        try {
+          const ban = await interaction.guild.bans.create(id, {
+            deleteMessageSeconds,
+            reason,
+          });
+
+          banned.push(ban);
+        } catch {
+          failed.push(userMention(id));
+        }
+
+        await interaction.editReply({
+          embeds: [
+            new EmbedBuilder()
+              .setDescription(failed.length ?
+                `❌ ${failed.join(" ")}`.slice(0, 4096) :
+                null)
+              .setFields([{
+                name: "Amount of banned users",
+                value: `${banned.length}/${ids.length}`,
+              }])
+              .setTitle("Ban Result"),
+          ],
+        }).catch(() => null);
+      }));
+
+      index += ids.length;
     }
 
-    const banned = await Promise.all(banning)
-      .then(bans => bans.filter(ban => ban));
+    await Promise.all(banning);
 
-    await interaction.update({
-      components: [],
+    await interaction.editReply({
+      content: null,
       embeds: [
         new EmbedBuilder()
-          .setDescription(failed.length ? `❌ ${failed.join(" ")}`.slice(0, 4096) : null)
+          .setDescription(failed.length ?
+            `❌ ${failed.join(" ")}`.slice(0, 4096) :
+            null)
           .setFields([{
             name: "Amount of banned users",
             value: `${banned.length}/${banned.length + failed.length}`,
