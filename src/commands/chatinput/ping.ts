f47f38ca10068fbcfabd6d9640981330b0b0774a
Gorniaky <hwss.s2@gmail.com>
2023-04-04 09:09:06 -0300

    ping show shards ping

--- src/commands/chatinput/ping.ts
+++ src/commands/chatinput/ping.ts
@@ -1,7 +1,11 @@
-import { Message } from "discord.js";
+import { AttachmentBuilder, Message, Status } from "discord.js";
+import client from "../../client";
 import Command from "../../structures/Command";
+import { makeMultiTable } from "../../util/utils";
 
 export default class extends Command {
+  [k: string]: any;
+
   constructor() {
     super({
       name: "ping",
@@ -11,10 +15,39 @@ export default class extends Command {
   async execute(message: Message) {
     const sent = await message.reply("Pong!");
 
+    if (message.args?.length) {
+      const fn = this[message.args[0].toLowerCase()];
+
+      if (fn) {
+        await fn(sent);
+        return;
+      }
+    }
+
     const ping = sent.createdTimestamp - (message.editedTimestamp ?? message.createdTimestamp);
 
-    await sent.edit(`Pong! \`API: ${message.client.ws.ping}ms\`, \`BOT: ${ping}ms\``);
+    await sent.edit(`Pong! \`API: ${client.ws.ping}ms\`, \`BOT: ${ping}ms\``);
 
     return;
+  }
+
+  async shards(sent: Message) {
+    const pingShards: string[][] = [["Shard", "Status", "Ping"]];
+
+    for (const shard of client.ws.shards.values()) {
+      pingShards.push([
+        `${shard.id}`,
+        `${Status[shard.status]}`,
+        `${shard.ping}`,
+      ]);
+    }
+
+    await sent.edit({
+      files: [
+        new AttachmentBuilder(Buffer.from(makeMultiTable(pingShards), "utf8"), {
+          name: "ping.r",
+        }),
+      ],
+    });
   }
 }
