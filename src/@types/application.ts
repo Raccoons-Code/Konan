fd736740dc42a9629b5ad1e62b82aae07ac821e1
Gorniaky <hwss.s2@gmail.com>
2023-04-10 12:35:37 -0300

    update types

--- src/@types/application.ts
+++ src/@types/application.ts
@@ -14,6 +14,7 @@ export interface Stats {
   emojis: number
   guilds: number
   interactions: number
+  cpuUsage: NodeJS.CpuUsage
   memoryUsage: NodeJS.MemoryUsage
   messages: number
   shardId: number
